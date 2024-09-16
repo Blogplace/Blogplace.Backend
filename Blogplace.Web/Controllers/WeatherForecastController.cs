4caceffff54486c635b9461711ccc135cdacd043
Marcin99b <marcin.bigos99@gmail.com>
2024-09-16 18:17:56 +0200

    editorconfig

--- Blogplace.Web/Controllers/WeatherForecastController.cs
+++ Blogplace.Web/Controllers/WeatherForecastController.cs
@@ -12,10 +12,7 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) => this._logger = logger;
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
--- Blogplace.Web/Program.cs
+++ Blogplace.Web/Program.cs
@@ -12,8 +12,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
