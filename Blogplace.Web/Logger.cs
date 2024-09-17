2a27dc974f9893115d17c9a77682f6021879ace1
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-09-17 22:07:50 +0200

    add serilog as default logger
    - Added new package Serilog.AspNetCore
    - Set serilog as the default app logger
    - Changed minimal log level for Microsoft.AspNetCore to Warning which enabled full SerilogRequestLogging potential

---
+++ Blogplace.Web/Logger.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+
+namespace Blogplace.Web;
+
+public class Logger
+{
+    public static Serilog.Core.Logger CreateDefaultAppLogger()
+    {
+        var loggerConfigBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var currentEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        if (currentEnvironment != null)
+        {
+            loggerConfigBuilder.AddJsonFile($"appsettings.{currentEnvironment}.json");
+        }
+
+        var loggerConfig = loggerConfigBuilder.Build();
+
+        return new LoggerConfiguration()
+            .ReadFrom.Configuration(loggerConfig)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+    }
+}
--- Blogplace.Web/Program.cs
+++ Blogplace.Web/Program.cs
@@ -1,28 +1,50 @@
-var builder = WebApplication.CreateBuilder(args);
+using Blogplace.Web;
+using Serilog;
 
-// Add services to the container.
+// Setup serilog as default logger
+Log.Logger = Logger.CreateDefaultAppLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    _ = app.UseSwagger();
-    _ = app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddSerilog();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        _ = app.UseSwagger();
+        _ = app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 public partial class Program
 {
