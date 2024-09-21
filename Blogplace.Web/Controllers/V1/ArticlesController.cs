caf00b88000bf283ac2e571b39f747aff85e0521
Marcin99b <marcin.bigos99@gmail.com>
2024-09-21 18:00:25 +0200

    controller

--- Blogplace.Web/Controllers/V1/ArticlesController.cs
+++ Blogplace.Web/Controllers/V1/ArticlesController.cs
@@ -1,8 +1,20 @@
-﻿using MediatR;
+﻿using Blogplace.Web.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Blogplace.Web.Controllers.V1;
 
 public sealed class ArticlesController(IMediator mediator) : V1ControllerBase
 {
+    public Task<CreateArticleResponse> Create(CreateArticleRequest request) => mediator.Send(request);
 
+    [AllowAnonymous]
+    public Task<GetArticleResponse> Get(GetArticleRequest request) => mediator.Send(request);
+
+    [AllowAnonymous]
+    public Task<SearchArticlesResponse> Search(SearchArticlesRequest request) => mediator.Send(request);
+
+    public Task Update(UpdateArticleRequest request) => mediator.Send(request);
+
+    public Task Delete(DeleteArticleRequest request) => mediator.Send(request);
 }
