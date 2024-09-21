0b424cc49bc3f49281fa6ea1f723e6c4cf2c3fb1
Marcin99b <marcin.bigos99@gmail.com>
2024-09-21 18:27:05 +0200

    fix registration

--- Blogplace.Web/ServiceExtensions.cs
+++ Blogplace.Web/ServiceExtensions.cs
@@ -21,7 +21,7 @@ public static class ServiceExtensions
         services.AddScoped<ISessionStorage, SessionStorage>();
 
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-        services.AddSingleton<IAuthorizationHandler, SessionCheckHandler>();
+        services.AddScoped<IAuthorizationHandler, SessionCheckHandler>();
         services.AddSingleton<IAuthManager, AuthManager>();
 
         services.AddAuthorizationBuilder()
