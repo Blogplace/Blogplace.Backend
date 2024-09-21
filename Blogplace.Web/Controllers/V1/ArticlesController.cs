82951d3a94e101108b97b592ca162e42fcc2f5f5
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-09-21 20:25:02 +0200

    fix swagger generator exception
    - Added HttpPost annotations to ArticlesController methods

--- Blogplace.Web/Controllers/V1/ArticlesController.cs
+++ Blogplace.Web/Controllers/V1/ArticlesController.cs
@@ -1,20 +1,26 @@
 ﻿using Blogplace.Web.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blogplace.Web.Controllers.V1;
 
 public sealed class ArticlesController(IMediator mediator) : V1ControllerBase
 {
+    [HttpPost]
     public Task<CreateArticleResponse> Create(CreateArticleRequest request) => mediator.Send(request);
 
     [AllowAnonymous]
+    [HttpPost]
     public Task<GetArticleResponse> Get(GetArticleRequest request) => mediator.Send(request);
 
     [AllowAnonymous]
+    [HttpPost]
     public Task<SearchArticlesResponse> Search(SearchArticlesRequest request) => mediator.Send(request);
 
+    [HttpPost]
     public Task Update(UpdateArticleRequest request) => mediator.Send(request);
 
+    [HttpPost]
     public Task Delete(DeleteArticleRequest request) => mediator.Send(request);
 }
