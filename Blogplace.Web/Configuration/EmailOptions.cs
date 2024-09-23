b97de88140f4befcc5f97b3c517bd1db36b00d26
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-09-23 19:34:00 +0200

    update default email port

--- Blogplace.Web/Configuration/EmailOptions.cs
+++ Blogplace.Web/Configuration/EmailOptions.cs
@@ -3,7 +3,7 @@
 public class EmailOptions
 {
     public string Host { get; set; } = "localhost";
-    public int Port { get; set; } = 587;
+    public int Port { get; set; } = 0;
     public required string User { get; set; }
     public string Password { get; set; } = "";
     public required string SenderEmail { get; set; }
