a0604ebd2543db778819856e5ad2e923f39cb8a8
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-09-24 20:25:55 +0200

    Create CommonPermissionsEnum.cs

---
+++ Blogplace.Web/Commons/CommonPermissionsEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace Blogplace.Web.Commons;
+
+[Flags]
+public enum CommonPermissionsEnum : int
+{
+    PostCreate = 1 << 0,
+    PostRead = 1 << 1,
+    PostUpdate = 1 << 2,
+    PostDelete = 1 << 3,
+
+    CommentCreate = 1 << 4,
+    CommentRead = 1 << 5,
+    CommentUpdate = 1 << 6,
+    CommentDelete = 1 << 7,
+
+    ProfileCreate = 1 << 8,
+    ProfileRead = 1 << 9,
+    ProfileUpdate = 1 << 10,
+    ProfileDelete = 1 << 11
+}
