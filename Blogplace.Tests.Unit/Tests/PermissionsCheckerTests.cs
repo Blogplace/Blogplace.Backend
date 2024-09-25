75de934308e527bf891d1d47eeafce3b169e1823
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-09-26 01:12:01 +0200

    add PermissionChecker unit tests

---
+++ Blogplace.Tests.Unit/Tests/PermissionsCheckerTests.cs
@@ -0,0 +1,28 @@
+﻿using Blogplace.Web.Auth;
+using Blogplace.Web.Commons;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogplace.Tests.Unit.Tests;
+
+internal class PermissionsCheckerTests
+{
+    [Test]
+    [TestCase(CommonPermissionsEnum.ArticleCreate, true)]
+    [TestCase(CommonPermissionsEnum.All & ~CommonPermissionsEnum.ArticleCreate, false)]
+    public Task PermissionsChecker_CanCreateArticle(CommonPermissionsEnum permissions, bool expected)
+    {
+        var permissionsChecker = new PermissionsChecker();
+
+        permissionsChecker
+            .CanCreateArticle(permissions)
+            .Should().Be(expected);
+                    
+        return Task.CompletedTask;
+    }
+}
+
