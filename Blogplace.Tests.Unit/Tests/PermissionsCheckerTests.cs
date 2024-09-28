04aea6e706a6ae8e583207f7c2f7fc6bf37c73dd
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-09-28 14:58:57 +0200

    fix PermissionsCheckerTests.cs

--- Blogplace.Tests.Unit/Tests/PermissionsCheckerTests.cs
+++ Blogplace.Tests.Unit/Tests/PermissionsCheckerTests.cs
@@ -9,43 +9,37 @@ internal class PermissionsCheckerTests
     [Test]
     [TestCase(CommonPermissionsEnum.ArticleCreate, true)]
     [TestCase(CommonPermissionsEnum.All & ~CommonPermissionsEnum.ArticleCreate, false)]
-    public Task PermissionsChecker_CanCreateArticle(CommonPermissionsEnum permissions, bool expected)
+    public void PermissionsChecker_CanCreateArticle(CommonPermissionsEnum permissions, bool expected)
     {
         var permissionsChecker = new PermissionsChecker();
 
         permissionsChecker
             .CanCreateArticle(permissions)
             .Should().Be(expected);
-                    
-        return Task.CompletedTask;
     }
 
     [Test]
     [TestCase(CommonPermissionsEnum.ArticleRead, true)]
     [TestCase(CommonPermissionsEnum.All & ~CommonPermissionsEnum.ArticleRead, false)]
-    public Task PermissionsChecker_CanReadArticle(CommonPermissionsEnum permissions, bool expected)
+    public void PermissionsChecker_CanReadArticle(CommonPermissionsEnum permissions, bool expected)
     {
         var permissionsChecker = new PermissionsChecker();
 
         permissionsChecker
             .CanReadArticle(permissions)
             .Should().Be(expected);
-
-        return Task.CompletedTask;
     }
 
     [Test]
     [TestCase(CommonPermissionsEnum.ArticleDelete, true)]
     [TestCase(CommonPermissionsEnum.All & ~CommonPermissionsEnum.ArticleDelete, false)]
-    public Task PermissionsChecker_CanDeleteArticle(CommonPermissionsEnum permissions, bool expected)
+    public void PermissionsChecker_CanDeleteArticle(CommonPermissionsEnum permissions, bool expected)
     {
         var permissionsChecker = new PermissionsChecker();
 
         permissionsChecker
             .CanDeleteArticle(permissions)
             .Should().Be(expected);
-
-        return Task.CompletedTask;
     }
 }
 
