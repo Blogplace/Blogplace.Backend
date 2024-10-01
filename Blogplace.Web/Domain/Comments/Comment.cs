ce7bc70ccec7d4b97071ecafed0ea5dd8bade242
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2024-10-01 23:28:41 +0200

    add Comment domain objects

---
+++ Blogplace.Web/Domain/Comments/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Blogplace.Web.Domain.Comments;
+
+public class Comment(Guid articleId, Guid authorId, string content, long? parentCommentId)
+{
+    public Guid Id { get; } = new Guid();
+    public Guid ArticleId { get; } = articleId;
+    public long? ParentId { get; } = parentCommentId;
+    public Guid AuthorId { get; } = authorId;
+    public string Content { get; } = content;
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Blogplace.Web/Domain/Comments/CommentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Blogplace.Web.Domain.Comments;
+
+// TODO: Replace GUID (where possible) with numerical ID for indexing optimization.
+public record CommentDto(
+    Guid Id,
+    Guid ArticleId,
+    long? ParentId,
+    Guid AuthorId,
+    string Content,
+    DateTime CreatedAt,
+    DateTime UpdatedAt
+    );
