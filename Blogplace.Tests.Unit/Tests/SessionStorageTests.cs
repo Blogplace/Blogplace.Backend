875c3f64b7f8a739ad9c076d65389e41278ed00e
Marcin99b <marcin.bigos99@gmail.com>
2024-10-10 15:41:32 +0200

    SetUserId_CanBeUsedOnlyOnce

---
+++ Blogplace.Tests.Unit/Tests/SessionStorageTests.cs
@@ -0,0 +1,15 @@
+﻿using Blogplace.Web.Auth;
+
+namespace Blogplace.Tests.Unit.Tests;
+
+[TestFixture]
+public class SessionStorageTests
+{
+    [Test]
+    public void SetUserId_CanBeUsedOnlyOnce()
+    {
+        var storage = new SessionStorage();
+        storage.SetUserId(Guid.NewGuid());
+        Assert.Throws<InvalidOperationException>(() => storage.SetUserId(Guid.NewGuid()));
+    }
+}
