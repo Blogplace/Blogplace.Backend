3c2a6be466ac0c8afee089e0c513be1b776dc2ca
Marcin99b <marcin.bigos99@gmail.com>
2024-11-18 22:56:22 +0100

    comment articles

--- Blogplace.Web/Controllers/V1/ArticlesController.cs
+++ Blogplace.Web/Controllers/V1/ArticlesController.cs
@@ -14,9 +14,9 @@ public sealed class ArticlesController(IMediator mediator) : V1ControllerBase
     //[HttpPost]
     //public Task<GetArticleResponse> Get(GetArticleRequest request) => mediator.Send(request);
 
-    [AllowAnonymous]
-    [HttpPost]
-    public Task<SearchArticlesResponse> Search(SearchArticlesRequest request) => mediator.Send(request);
+    //[AllowAnonymous]
+    //[HttpPost]
+    //public Task<SearchArticlesResponse> Search(SearchArticlesRequest request) => mediator.Send(request);
 
     //[HttpPost]
     //public Task Update(UpdateArticleRequest request) => mediator.Send(request);
--- Blogplace.Web/Domain/Articles/Requests/SearchArticlesRequestHandler.cs
+++ Blogplace.Web/Domain/Articles/Requests/SearchArticlesRequestHandler.cs
@@ -1,19 +1,19 @@
-﻿using Blogplace.Web.Infrastructure.Database;
-using MediatR;
+﻿//using Blogplace.Web.Infrastructure.Database;
+//using MediatR;
 
-namespace Blogplace.Web.Domain.Articles.Requests;
+//namespace Blogplace.Web.Domain.Articles.Requests;
 
-public record SearchArticlesResponse(IEnumerable<ArticleSmallDto> Articles); //todo light dtos
-public record SearchArticlesRequest(Guid? TagId = null) : IRequest<SearchArticlesResponse>; //todo filters
-public class SearchArticlesRequestHandler(IArticlesRepository repository) : IRequestHandler<SearchArticlesRequest, SearchArticlesResponse>
-{
-    //todo config
-    private const int SEARCH_LIMIT = 10;
+//public record SearchArticlesResponse(IEnumerable<ArticleSmallDto> Articles); //todo light dtos
+//public record SearchArticlesRequest(Guid? TagId = null) : IRequest<SearchArticlesResponse>; //todo filters
+//public class SearchArticlesRequestHandler(IArticlesRepository repository) : IRequestHandler<SearchArticlesRequest, SearchArticlesResponse>
+//{
+//    //todo config
+//    private const int SEARCH_LIMIT = 10;
 
-    public async Task<SearchArticlesResponse> Handle(SearchArticlesRequest request, CancellationToken cancellationToken)
-    {
-        var results = await repository.Search(SEARCH_LIMIT, request.TagId);
-        var dtos = results.Select(x => new ArticleSmallDto(x.Id, x.Url, x.Title, x.Views, x.TagIds, x.CreatedAt, x.UpdatedAt));
-        return new SearchArticlesResponse(dtos);
-    }
-}
+//    public async Task<SearchArticlesResponse> Handle(SearchArticlesRequest request, CancellationToken cancellationToken)
+//    {
+//        var results = await repository.Search(SEARCH_LIMIT, request.TagId);
+//        var dtos = results.Select(x => new ArticleSmallDto(x.Id, x.Url, x.Title, x.Views, x.TagIds, x.CreatedAt, x.UpdatedAt));
+//        return new SearchArticlesResponse(dtos);
+//    }
+//}
