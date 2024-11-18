55d05886c5365c60733f642a7e3be432faee8b06
Marcin99b <marcin.bigos99@gmail.com>
2024-11-18 22:02:11 +0100

    download feed

--- Blogplace.Web/Background/Jobs/ImportBlogArticlesJob.cs
+++ Blogplace.Web/Background/Jobs/ImportBlogArticlesJob.cs
@@ -1,14 +1,53 @@
-﻿namespace Blogplace.Web.Background.Jobs;
+﻿using Blogplace.Web.Infrastructure.Database;
+using System.Xml;
+using System;
+using System.ServiceModel.Syndication;
 
-public class ImportBlogArticlesJob : IJob
+namespace Blogplace.Web.Background.Jobs;
+
+public class ImportBlogArticlesJob(IArticlesRepository articlesRepository) : IJob
 {
-    public Task Run()
+    private readonly HttpClient client = new();
+
+    private readonly Uri[] feeds = 
+    [
+        //todo database list
+        new Uri("https://techcrunch.com/feed/"),
+        new Uri("https://lifehacker.com/feed/rss"),
+        new Uri("http://feeds.hbr.org/harvardbusiness"),
+        new Uri("https://www.entrepreneur.com/latest.rss"),
+        new Uri("https://copyblogger.com/feed/"),
+    ];
+
+    public async Task Run()
     {
-        return Task.CompletedTask;
+        foreach (var feed in this.feeds)
+        {
+            try
+            {
+                await this.Import(feed, DateTime.UtcNow.Date);
+            }
+            catch //todo
+            {
+            }
+        }
     }
-}
 
-public interface IJob
-{
-    Task Run();
+    private async Task Import(Uri source, DateTime lastUpdateUtc)
+    {
+        var rssStream = await (await this.client.GetAsync(source)).Content.ReadAsStreamAsync();
+        using var reader = XmlReader.Create(rssStream);
+        var feed = SyndicationFeed.Load(reader);
+
+        if (lastUpdateUtc > feed.LastUpdatedTime.UtcDateTime)
+        {
+            return;
+        }
+
+        foreach (var article in feed.Items.Where(x => lastUpdateUtc > x.LastUpdatedTime.UtcDateTime))
+        {
+            var id = article.Id;
+            //todo change article model
+        }
+    }
 }
