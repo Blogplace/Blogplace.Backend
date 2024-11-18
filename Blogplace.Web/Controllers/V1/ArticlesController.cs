1e9596bb4a82666c4c09df17305631f1f1eaf04b
Marcin99b <marcin.bigos99@gmail.com>
2024-11-18 22:55:19 +0100

    comment posts and tags

--- Blogplace.Web/Controllers/V1/ArticlesController.cs
+++ Blogplace.Web/Controllers/V1/ArticlesController.cs
@@ -10,16 +10,16 @@ public sealed class ArticlesController(IMediator mediator) : V1ControllerBase
     //[HttpPost]
     //public Task<CreateArticleResponse> Create(CreateArticleRequest request) => mediator.Send(request);
 
-    [AllowAnonymous]
-    [HttpPost]
-    public Task<GetArticleResponse> Get(GetArticleRequest request) => mediator.Send(request);
+    //[AllowAnonymous]
+    //[HttpPost]
+    //public Task<GetArticleResponse> Get(GetArticleRequest request) => mediator.Send(request);
 
     [AllowAnonymous]
     [HttpPost]
     public Task<SearchArticlesResponse> Search(SearchArticlesRequest request) => mediator.Send(request);
 
-    [HttpPost]
-    public Task Update(UpdateArticleRequest request) => mediator.Send(request);
+    //[HttpPost]
+    //public Task Update(UpdateArticleRequest request) => mediator.Send(request);
 
     //[HttpPost]
     //public Task Delete(DeleteArticleRequest request) => mediator.Send(request);
@@ -28,11 +28,11 @@ public sealed class ArticlesController(IMediator mediator) : V1ControllerBase
     //[HttpPost]
     //public Task View(ViewArticleRequest request) => mediator.Send(request);
 
-    [AllowAnonymous]
-    [HttpPost]
-    public Task<SearchTagsResponse> SearchTags(SearchTagsRequest request) => mediator.Send(request);
+    //[AllowAnonymous]
+    //[HttpPost]
+    //public Task<SearchTagsResponse> SearchTags(SearchTagsRequest request) => mediator.Send(request);
 
-    [AllowAnonymous]
-    [HttpPost]
-    public Task<GetTagsByIdsResponse> GetTagsByIds(GetTagsByIdsRequest request) => mediator.Send(request);
+    //[AllowAnonymous]
+    //[HttpPost]
+    //public Task<GetTagsByIdsResponse> GetTagsByIds(GetTagsByIdsRequest request) => mediator.Send(request);
 }
--- Blogplace.Web/Domain/Articles/Requests/UpdateArticleRequestHandler.cs
+++ Blogplace.Web/Domain/Articles/Requests/UpdateArticleRequestHandler.cs
@@ -1,74 +1,74 @@
-﻿using Blogplace.Web.Auth;
-using Blogplace.Web.Background;
-using Blogplace.Web.Commons.Logging;
-using Blogplace.Web.Exceptions;
-using Blogplace.Web.Infrastructure.Database;
-using MediatR;
+﻿//using Blogplace.Web.Auth;
+//using Blogplace.Web.Background;
+//using Blogplace.Web.Commons.Logging;
+//using Blogplace.Web.Exceptions;
+//using Blogplace.Web.Infrastructure.Database;
+//using MediatR;
 
-namespace Blogplace.Web.Domain.Articles.Requests;
+//namespace Blogplace.Web.Domain.Articles.Requests;
 
-public record UpdateArticleRequest(string Id, string? NewTitle = null, string? NewContent = null, string[]? Tags = null) : IRequest;
+//public record UpdateArticleRequest(string Id, string? NewTitle = null, string? NewContent = null, string[]? Tags = null) : IRequest;
 
-public class UpdateArticleRequestHandler(
-    ISessionStorage sessionStorage,
-    IArticlesRepository repository,
-    IUsersRepository usersRepository,
-    ITagsRepository tagsRepository,
-    IPermissionsChecker permissionsChecker,
-    IEventLogger logger,
-    ITagsCleaningChannel tagsCleaningChannel
-) : IRequestHandler<UpdateArticleRequest>
-{
-    public async Task Handle(UpdateArticleRequest request, CancellationToken cancellationToken)
-    {
-        var user = await usersRepository.Get(sessionStorage.UserId);
-        if (!permissionsChecker.CanUpdateArticle(user.Permissions))
-        {
-            throw new UserPermissionDeniedException("No permission to update the article");
-        }
+//public class UpdateArticleRequestHandler(
+//    ISessionStorage sessionStorage,
+//    IArticlesRepository repository,
+//    IUsersRepository usersRepository,
+//    ITagsRepository tagsRepository,
+//    IPermissionsChecker permissionsChecker,
+//    IEventLogger logger,
+//    ITagsCleaningChannel tagsCleaningChannel
+//) : IRequestHandler<UpdateArticleRequest>
+//{
+//    public async Task Handle(UpdateArticleRequest request, CancellationToken cancellationToken)
+//    {
+//        var user = await usersRepository.Get(sessionStorage.UserId);
+//        if (!permissionsChecker.CanUpdateArticle(user.Permissions))
+//        {
+//            throw new UserPermissionDeniedException("No permission to update the article");
+//        }
         
-        var isChanged = false;
-        //todo get only author of article
-        var article = await repository.Get(request.Id);
+//        var isChanged = false;
+//        //todo get only author of article
+//        var article = await repository.Get(request.Id);
 
-        //if (article.AuthorId != sessionStorage.UserId)
-        //{
-        //    throw new UserPermissionDeniedException("Requester is not author of article");
-        //}
+//        //if (article.AuthorId != sessionStorage.UserId)
+//        //{
+//        //    throw new UserPermissionDeniedException("Requester is not author of article");
+//        //}
 
-        if (request.NewTitle != null)
-        {
-            article.Title = request.NewTitle;
-            isChanged = true;
-        }
+//        if (request.NewTitle != null)
+//        {
+//            article.Title = request.NewTitle;
+//            isChanged = true;
+//        }
 
-        if (request.NewContent != null)
-        {
-            article.Content = request.NewContent;
-            isChanged = true;
-        }
+//        if (request.NewContent != null)
+//        {
+//            article.Content = request.NewContent;
+//            isChanged = true;
+//        }
 
-        if(request.Tags != null)
-        {
-            await tagsRepository.AddIfNotExists(request.Tags);
-            var tagsIds = (await tagsRepository.Get(request.Tags)).Select(x => x.Id).ToList();
+//        if(request.Tags != null)
+//        {
+//            await tagsRepository.AddIfNotExists(request.Tags);
+//            var tagsIds = (await tagsRepository.Get(request.Tags)).Select(x => x.Id).ToList();
 
-            var tagsDeletedFromArticle = article.TagIds.Where(x => !tagsIds.Contains(x)).ToArray();
-            article.TagIds = tagsIds;
+//            var tagsDeletedFromArticle = article.TagIds.Where(x => !tagsIds.Contains(x)).ToArray();
+//            article.TagIds = tagsIds;
 
-            foreach (var tagToCheck in tagsDeletedFromArticle)
-            {
-                //todo do only if repository.Update() succeed
-                await tagsCleaningChannel.Publish(tagToCheck);
-            }
+//            foreach (var tagToCheck in tagsDeletedFromArticle)
+//            {
+//                //todo do only if repository.Update() succeed
+//                await tagsCleaningChannel.Publish(tagToCheck);
+//            }
 
-            isChanged = true;
-        }
+//            isChanged = true;
+//        }
 
-        if (isChanged)
-        {
-            await repository.Update(article);
-            logger.UserUpdatedArticle(sessionStorage.UserId, article.Id);
-        }
-    }
-}
+//        if (isChanged)
+//        {
+//            await repository.Update(article);
+//            logger.UserUpdatedArticle(sessionStorage.UserId, article.Id);
+//        }
+//    }
+//}
